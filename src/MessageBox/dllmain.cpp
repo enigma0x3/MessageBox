a240ca7c609acf76a0f56a91b9d81d62ad032240
enigma0x3 <matthew@webnelson.net>
2016-07-20 17:43:26 -0400

    Included integrity checks

--- src/MessageBox/dllmain.cpp
+++ src/MessageBox/dllmain.cpp
@@ -1,5 +1,22 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "stdafx.h"
+#include <windows.h>
+
+BOOL IsElevated() {
+	BOOL fRet = FALSE;
+	HANDLE hToken = NULL;
+	if (OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken)) {
+		TOKEN_ELEVATION Elevation;
+		DWORD cbSize = sizeof(TOKEN_ELEVATION);
+		if (GetTokenInformation(hToken, TokenElevation, &Elevation, sizeof(Elevation), &cbSize)) {
+			fRet = Elevation.TokenIsElevated;
+		}
+	}
+	if (hToken) {
+		CloseHandle(hToken);
+	}
+	return fRet;
+}
 
 BOOL APIENTRY DllMain(HMODULE hModule,
 	DWORD  ul_reason_for_call,
@@ -9,8 +26,16 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-		MessageBox(0, L"pwn", 0, 0);
-		break;
+		BOOL isAdmin;
+		isAdmin = IsElevated();
+		if (isAdmin) {
+			MessageBox(0, L"High Integrity!", 0, 0);
+			break;
+		}
+		else {
+			MessageBox(0, L"Not High Integrity", 0, 0);
+			break;
+		}
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
 	case DLL_PROCESS_DETACH:
